3adbad11c249759007b64b6b469c1bf8dfb7e747
Andrew G <gountisa@roverkids.org>
2026-04-28 12:40:03 -0400

    Changed the color and font of the text inside the home menu

--- Assets/_Scripts/HomeRunner.cs
+++ Assets/_Scripts/HomeRunner.cs
@@ -16,4 +16,12 @@ public class HomeRunner : MonoBehaviour
     {
         GameEvents.CloseSettingsMenu(new ClosedSettingsMenuEventArgs(FindObjectOfType<SettingsManager>().playerSettings));
     }
+    public void CloseGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
 }
